f7080d3b498832b507543afac03bc6f0664ddd98
mland_laptop <mland@DESKTOP-PNEL2RP>
2016-11-11 15:07:03 -0500

    added Book component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Book from './components/Book';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <h1>Hello, world, my prop is {this.props.foo}</h1>
+      <Book />
       </div>
     );
   }
---
+++ src/components/Book.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Book extends Component {
+  render() {
+    return (
+      <div className="Book">
+      <p>Im a Book</p>
+      </div>
+    );
+  }
+}
+
+export default Book;
